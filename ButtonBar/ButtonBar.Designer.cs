ae1e796b74d214739a97322f620dc21d7b914ea0
cobolman <d2@webgate.ltd.uk>
2016-02-12 08:26:54 +0000

    Initial check-in

---
+++ ButtonBar/ButtonBar.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ButtonBar
+{
+    partial class ButtonBar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPrior = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPrior
+            // 
+            this.btnPrior.Location = new System.Drawing.Point(560, 0);
+            this.btnPrior.Name = "btnPrior";
+            this.btnPrior.Size = new System.Drawing.Size(20, 20);
+            this.btnPrior.TabIndex = 0;
+            this.btnPrior.Text = "<";
+            this.btnPrior.UseVisualStyleBackColor = true;
+            this.btnPrior.Click += new System.EventHandler(this.btnPrior_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(580, 0);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(20, 20);
+            this.btnNext.TabIndex = 1;
+            this.btnNext.Text = ">";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrior);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(600, 38);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPrior;
+        private System.Windows.Forms.Button btnNext;
+    }
+}
---
+++ ButtonBar/ButtonBar.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ButtonBar
+{
+    public partial class ButtonBar: UserControl
+    {
+        int btnWidth = 74;
+        int btnHeight = 38;
+
+        [Browsable(false)]
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public List<Button> Buttons
+        { get; set; }
+
+        public int Count
+        { 
+            get { return Buttons.Count; }
+        }
+
+        public Button GetButton(int index)
+        {
+            if (index >= 0 && index < Buttons.Count)
+                return Buttons[index];
+            else
+                return null;
+        }
+
+        public event EventHandler ButtonPush;
+
+        public ButtonBar()
+        {
+            InitializeComponent();
+            Buttons = new List<Button>();
+            InitializeButtons();
+        }
+
+        private void ShowHidePriorNextButtons()
+        {
+            if (Buttons.Count * btnWidth < this.Width)
+            {
+                btnPrior.Visible = false;
+                btnNext.Visible = false;
+            }
+            else
+            {
+                btnPrior.Visible = true;
+                btnNext.Visible = true;
+            }
+        }
+
+        public void Add(String btnLabel)
+        {
+            Button btn = new Button();
+
+            btn.Location = new Point(Buttons.Count * btnWidth, 0);
+            btn.Size = new System.Drawing.Size(btnWidth, btnHeight);
+            btn.Text = btnLabel.ToUpper();
+
+            btn.TabIndex = Buttons.Count * 10;
+            btn.Font = new Font(btn.Font, FontStyle.Bold);
+            btn.Click += HandleButtonPush;
+
+            Buttons.Add(btn);
+            this.Controls.Add(btn);
+            ShowHidePriorNextButtons();
+        }
+
+        public void Remove(Button item)
+        {
+            Buttons.Remove(item);
+
+            for (int n = 0; n < Buttons.Count; n++ )
+            {
+                Buttons[n].Location = new Point(n * btnWidth, 0);
+            }
+
+            ShowHidePriorNextButtons();
+        }
+
+        private void InitializeButtons()
+        {
+            ShowHidePriorNextButtons();
+        }
+
+        private void HandleButtonPush(object sender, EventArgs e)
+        {
+            OnButtonPush(sender, e);
+        }
+
+        protected virtual void OnButtonPush(object sender, EventArgs e)
+        {
+            EventHandler handler = ButtonPush;
+            if (handler != null)
+            {
+                handler(sender, e);
+            }
+        }
+
+
+        private void btnPrior_Click(object sender, EventArgs e)
+        {
+            if (Buttons[0].Location.X != 0)
+            {
+                for (int n = 0; n < Buttons.Count; n++)
+                {
+                    int x = Buttons[n].Location.X;
+                    int y = Buttons[n].Location.Y;
+                    Buttons[n].Location = new Point(x + 74, y);
+                }
+            }
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (Buttons[Buttons.Count-1].Location.X > 444)
+            {
+                for (int n = 0; n < Buttons.Count; n++)
+                {
+                    int x = Buttons[n].Location.X;
+                    int y = Buttons[n].Location.Y;
+                    Buttons[n].Location = new Point(x - 74, y);
+                }
+            }
+        }
+    }
+}
